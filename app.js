3df7cb72c33c2a3d4db4ade2b9709f789e4ba035
Mehdi Salhi <mehdisalhi.dev@gmail.com>
2026-06-08 10:08:04 -0400

    Module 5 completed baseline

--- app.js
+++ app.js
@@ -8,7 +8,11 @@ var indexRouter = require('./app_server/routes/index');
 var usersRouter = require('./app_server/routes/users');
 var travelRouter = require('./app_server/routes/travel');
 var handlebars = require('hbs');
-require('./app_server/models/db');
+var apiRouter = require('./app_api/routes/index');
+
+
+require('./app_api/models/db');
+
 
 
 var app = express();
@@ -28,6 +32,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/travel', travelRouter);
+app.use('/api', apiRouter);
+
 
 
 // catch 404 and forward to error handler
