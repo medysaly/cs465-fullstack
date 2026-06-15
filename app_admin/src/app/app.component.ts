4cfcfa93811578e32a77d29548e13c53927bfa01
Mehdi Salhi <mehdisalhi.dev@gmail.com>
2026-06-15 18:50:30 -0400

    Module 6 Completed Baseline

---
+++ app_admin/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { TripListingComponent } from './trip-listing/trip-listing.component';
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, TripListingComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'Travlr Getaways Admin!';
+}
